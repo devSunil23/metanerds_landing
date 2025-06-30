3399d4ccfc0d2b5ced7e47f2f827fec96fa361ef
dvlprsunil <dvlprsunil@gmail.com>
2025-06-30 22:27:56 +0530

    leadership section fixed

--- src/components/ui/LeadershipSection.jsx
+++ src/components/ui/LeadershipSection.jsx
@@ -5,21 +5,20 @@ import Image from "next/image";
 
 const LeadershipSection = () => {
   return (
-    <section className="relative pt-48 bg-black text-white py-12 px-6 md:px-18 lg:px-48 font-jakarta overflow-hidden">
+    <section className="relative bg-black text-white pt-48 pb-12 px-6 md:px-18 lg:px-48 font-jakarta overflow-hidden">
       {/* Background Image */}
       <div className="absolute inset-0 z-0">
         <Image
-          src="/assets/leadership-bg.png" // Update this path
+          src="/assets/leadership-bg.png"
           alt="Leadership background"
-          layout="fill"
-          objectFit="cover"
-          className="opacity-30"
+          fill
+          className="object-cover opacity-30"
         />
       </div>
 
       {/* Foreground Content */}
-      <div className="relative  z-10 max-w-7xl mx-auto flex flex-col-reverse md:flex-row items-center justify-between gap-12">
-        {/* === Left Content === */}
+      <div className="relative z-10 max-w-7xl mx-auto -mt-30 flex flex-col-reverse md:flex-row items-center justify-between gap-12">
+        {/* Left Content */}
         <div className="w-full md:w-1/2 text-left">
           <h2
             className="text-3xl md:text-5xl font-extrabold uppercase mb-4"
@@ -32,12 +31,12 @@ const LeadershipSection = () => {
             <h3 className="text-xl md:text-2xl font-semibold text-[#A7AAFF]">
               Shavez Ahmed Siddiqui
             </h3>
-            <p className="my-2 text-sm md:text-sms font-extrasemibold italic text-[#FFFFFF">
+            <p className="my-2 text-sm md:text-sm font-semibold italic text-white">
               Managing Partner – Digital Systems & Fintech Innovation
             </p>
           </div>
 
-          <ul className="list-disc list-inside mt-4 text-sm md:text-sm text-[#FFFFFFCC ] font-extralight leading-relaxed">
+          <ul className="list-disc list-inside mt-4 text-sm text-white/80 font-extralight leading-relaxed">
             <li>
               Brings a decade of experience building high-integrity platforms
               across finance and tech sectors.
@@ -45,7 +44,7 @@ const LeadershipSection = () => {
           </ul>
         </div>
 
-        {/* === Right Image === */}
+        {/* Right Image */}
         <div className="w-full md:w-1/2 flex justify-center">
           <div className="relative w-[280px] md:w-[400px] aspect-[2/3] rounded-2xl overflow-hidden drop-shadow-2xl z-20">
             <Image
